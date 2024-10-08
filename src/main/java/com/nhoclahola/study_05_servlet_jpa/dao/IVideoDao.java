69d92ba7b0c51da04f24bb7b358c882fb9f60be9
nhoclahola <nhoclahola@gmail.com>
2024-10-08 07:13:06 +0700

    Update VideoDao

--- src/main/java/com/nhoclahola/study_05_servlet_jpa/dao/IVideoDao.java
+++ src/main/java/com/nhoclahola/study_05_servlet_jpa/dao/IVideoDao.java
@@ -1,5 +1,22 @@
 package com.nhoclahola.study_05_servlet_jpa.dao;
 
+import com.nhoclahola.study_05_servlet_jpa.entities.Video;
+
+import java.util.List;
+
 public interface IVideoDao
 {
+    void insert(Video video);
+
+    void update(Video video);
+
+    void delete(int videoId) throws Exception;
+
+    Video findById(int videoId);
+
+    List<Video> findAll();
+
+    List<Video> findAll(int page, int pagesize);
+
+    int count();
 }
--- src/main/java/com/nhoclahola/study_05_servlet_jpa/dao/implement/VideoDao.java
+++ src/main/java/com/nhoclahola/study_05_servlet_jpa/dao/implement/VideoDao.java
@@ -1,5 +1,129 @@
 package com.nhoclahola.study_05_servlet_jpa.dao.implement;
 
-public class VideoDao
+import com.nhoclahola.study_05_servlet_jpa.configurations.JPAConfig;
+import com.nhoclahola.study_05_servlet_jpa.dao.IVideoDao;
+import com.nhoclahola.study_05_servlet_jpa.entities.Video;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
+
+public class VideoDao implements IVideoDao
 {
+    @Override
+    public void insert(Video video)
+    {
+        EntityManager enma = JPAConfig.getEntityManager();
+        EntityTransaction trans = enma.getTransaction();
+        try
+        {
+            trans.begin();
+            //gọi phuong thức để insert, update, delete
+            enma.persist(video);
+            trans.commit();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            trans.rollback();
+            throw e;
+        }
+        finally
+        {
+            enma.close();
+        }
+    }
+
+    @Override
+    public void update(Video video)
+    {
+        EntityManager enma = JPAConfig.getEntityManager();
+        EntityTransaction trans = enma.getTransaction();
+        try
+        {
+            trans.begin();
+            //gọi phuong thức để insert, update, delete
+            enma.merge(video);
+            trans.commit();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            trans.rollback();
+            throw e;
+        }
+        finally
+        {
+            enma.close();
+        }
+    }
+
+    @Override
+    public void delete(int videoId) throws Exception
+    {
+        EntityManager enma = JPAConfig.getEntityManager();
+        EntityTransaction trans = enma.getTransaction();
+        try
+        {
+            trans.begin();
+            //gọi phuong thức để insert, update, delete
+            Video video = enma.find(Video.class, videoId);
+            if (video != null)
+            {
+                enma.remove(video);
+            }
+            else
+            {
+                throw new Exception("Không tìm thấy");
+            }
+            trans.commit();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            trans.rollback();
+            throw e;
+        }
+        finally
+        {
+            enma.close();
+        }
+    }
+
+    @Override
+    public Video findById(int videoId)
+    {
+        EntityManager enma = JPAConfig.getEntityManager();
+        Video video = enma.find(Video.class, videoId);
+        return video;
+    }
+
+    @Override
+    public List<Video> findAll()
+    {
+        EntityManager enma = JPAConfig.getEntityManager();
+        TypedQuery<Video> query = enma.createNamedQuery("Video.findAll", Video.class);
+        return query.getResultList();
+    }
+
+    @Override
+    public List<Video> findAll(int page, int pagesize)
+    {
+        EntityManager enma = JPAConfig.getEntityManager();
+        TypedQuery<Video> query = enma.createNamedQuery("Video.findAll", Video.class);
+        query.setFirstResult(page * pagesize);
+        query.setMaxResults(pagesize);
+        return query.getResultList();
+    }
+
+    @Override
+    public int count()
+    {
+        EntityManager enma = JPAConfig.getEntityManager();
+        String jpql = "SELECT count(c) FROM Video v";
+        Query query = enma.createQuery(jpql);
+        return ((Long) query.getSingleResult()).intValue();
+    }
 }
